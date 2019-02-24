065850b1fc3a878a2ba8cfeaa1ddd6d52862e599
Julie Freund-Poulsen <juliefreund-poulsen@macbook-pro-5.home>
2019-02-24 23:19:17 +0100

    burgermenu done

---
+++ egen restaurant/script.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", sidenVises);
+//DomReady.ready(function () {
+//    alert('dom is ready');
+//});
+
+function sidenVises() {
+    console.log("siden vises");
+    //registrer klik på menu-knap
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector("#burgermenu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#burgermenu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        //menuen er nu skjult - ændrer menuknap til burgermenu
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        //menuen er nu vist - ændrer menuknap til kryds
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
+
+
+
